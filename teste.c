2160fcca7abdd4a6901d165f407b83996e26d2cb
Victor Costa <victorcostaohhc@gmail.com>
2021-08-01 15:29:45 -0300

    Ola mundo com comentários sobre a função

--- teste.c
+++ teste.c
@@ -1,7 +1,9 @@
-#include <unistd.h>
+#include <unistd.h> // biblioteca Unix Standard, necessária para a execução da função write.
 
-int main(void)
-{
-    write (1, "Ola Mundo!\n", 11);
-    return (0);
+int main(void) // função main, ou função de entrada, do tipo inteiro (int) com a saída vazia (void)
+{ // colchetes necessários para abrir e fechar o escopo pretendido da função.
+    write (1, "Ola Mundo!\n", 11); // função write, onde o primeiro parâmetro significa a saída padrão, o segundo a série de caracteres que irão ser escritos e a terceira o número de bytes total. O caractere de quebra de linha "\n" equivale a um único byte.
+    return (0); // especificando o retorno vazio da função.
 }
+
+// caso deseje validar esse comando, abra no terminal e digite 'gcc teste.c' sem as aspas e em seguida aperte a tecla enter, assim o compilador irá transformar esse código para o mesmo ser legível para a máquina. Ao compila-lo, o mesmo gerará um arquivo a.out, do qual pode ser executado com o comando no terminal 'a.out', do qual executará a mensagem de "Ola Mundo!" 
