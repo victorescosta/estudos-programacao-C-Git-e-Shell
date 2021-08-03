3d3a556dad183fbe2132d096c99f14ccf24883bc
Victor Costa <victorcostaohhc@gmail.com>
2021-08-02 21:16:56 -0300

    last changes for today - maybe. a few new tests

--- teststr.c
+++ teststr.c
@@ -1,17 +1,17 @@
 #include <stdio.h> // biblioteca padrão, contém a função printf
 #include <unistd.h> // biblioteca Unix, contém a função write
 
-int main(void) 
+int main(void) // declaração da função principal (entry function) 
 {
-    char c;
-    int i;
+    char c; // declaração da variável 'c' de tipo caractere
+    int i; // declaração da variável 'i' de tipo inteiro
     
     char str[]="Hello again!"; /* cadeia de caracteres declarada assim:*str 
     é implicitada como const. ver declaração sobre a falha de segmentação*/
     char str2[]="Hello world!"; // formato alterável, sem const
 
     str2[4]= 65; // alteração específica de caractere da string via ASCII code
-    str[4]= 66; /* falha de segmentação, pois desejou-se alterar uma variável
+    str[4]= 76; /* falha de segmentação, pois desejou-se alterar uma variável
     do tipo const. Para resolver o erro, retira-se o '*' da mesma */
     c = 'c';
     
@@ -21,10 +21,12 @@ int main(void)
     }
     write(1, "\n", 1);
     write(1, "Using UNIX function: Goodbye, World!\n", 37);
-    printf("Print only a string (str): %s\n", str);
+    printf("Print only a string (str): %s\n", str); /* especificador de 
+    formatação %s para formatar string.*/
     printf("Print only a string (str2): %s\n", str2);
     printf("Goodbye, World!\n");
-    printf("Print only a char: %c\n", c);
+    printf("Print only a char: %c\n", c); /* especificador de 
+    formatação %c para formatar caractere.*/
 
-    return (0);
+    return (0); // retorno vazio para a função. já é 0 por padrão
 }
