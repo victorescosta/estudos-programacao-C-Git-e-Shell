a1c75b18bfd9890feecf248b8749db36c7e2c5b9
Victor Costa <victorcostaohhc@gmail.com>
2021-08-03 23:36:11 -0300

    ex00 - done

---
+++ refazendo/c00/ex00/ft_putchar.c
@@ -0,0 +1,15 @@
+// Escreva uma função que mostre o caractere (char) passado como parâmetro.
+// Função autorizada: write
+#include <unistd.h> // Unix standard vrs 5. Necessária para declarar a função write.
+void ft_putchar(char c); // protótipo esperado
+
+int main(void) // main para validar a função
+{
+    ft_putchar('b'); // atribuição de parâmetro da função para testar saída
+    return (0); // retorno vazio, mas necessário explicitar para adequar às normas da norminette
+}
+
+void ft_putchar(char c) // declarando a função tipo void, com parâmetro char c.
+{
+    write (1, &c, 1); // 1º par saída padrão, 2º ender. da var, 3º bytes number
+}
